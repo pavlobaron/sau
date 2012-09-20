24fb9b04713b6b796330e33a2ec55d65da1602a5
pavlobaron <pb@pbit.org>
2012-09-20 21:40:42 +0200

    removed antlr stuff because it just doesn't work. Back to flex/bison

--- lib/sau/sau_processor.py
+++ lib/sau/sau_processor.py
@@ -1,17 +1,3 @@
-from antlr3 import ANTLRStringStream
-from antlr3 import CommonTokenStream
-from antlr3 import tree
-from QueryParser import QueryParser
-from QueryLexer import QueryLexer
-
 class SauProcessor(object):
     def execute(self, line):
-        s = line.encode('utf8')
-
-        stream = ANTLRStringStream(s)
-        lexer = QueryLexer(stream)
-        tokens = CommonTokenStream(lexer)
-        parser = QueryParser(tokens)
-        q = parser.query()
-        
-        print "aaaaa   %s" % q
+        print "line   %s" % line
